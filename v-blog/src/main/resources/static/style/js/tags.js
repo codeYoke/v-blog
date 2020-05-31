8543307706f734c2a5b737d92a7f72b189c0a684
fjh <920766616@qq.com>
2020-06-01 02:29:14 +0800

    增加了一个有趣的音乐插件

--- v-blog/src/main/resources/static/style/js/tags.js
+++ v-blog/src/main/resources/static/style/js/tags.js
@@ -34,3 +34,40 @@ $.ajax({
         alert("出错啦...");
     }
 })
+
+/*获取最近文章*/
+/*
+
+function putInLabel(data){
+    var length = data.length;
+    $(".hao-div-div").empty();
+    var tagVaues = $(".hao-div-div");
+    var center = $(
+        '<div class="am-g">' +
+        ' <h1 class="blog-title"><i>标签云</i></h1>' +
+        '<hr>' +
+        '<div class="am-u-sm-12 blog-clear-padding">' +
+
+        '</div>' +
+        '</div>');
+    tagVaues.append(center);
+    $.each(data, function (index, obj) {
+        var cent = $('<a href="/tags?tag=' + obj['labelName'] + '" class="blog-tag am-kai">' + obj['labelName'] + '&nbsp;&nbsp;</a>');
+        $(".blog-clear-padding").append(cent);
+    })
+}
+$.ajax({
+    type: "GET",
+    url: "/tag/getTags",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (data) {
+        //放入数据
+        if(data.status == 200){
+            putInLabel(data.data);
+        }
+    },
+    error: function () {
+        alert("出错啦...");
+    }
+})*/
